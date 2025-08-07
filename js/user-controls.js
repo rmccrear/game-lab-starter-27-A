e242fea08a42e67a6d4173da58f5dcc91daa0dab
Alphonso Gillis <shadowtheguy912@gmail.com>
2025-08-07 17:37:35 -0500

    Changed jump to work a bit better, and fixed couch collision

--- js/user-controls.js
+++ js/user-controls.js
@@ -2,11 +2,15 @@
 function respondToUser() {
 
     if(keyWentDown("UP")){
-        player.velocityY=-4
+        player.velocityY=-6
     }
 
     if(player.velocityY<4){
-        player.velocityY = player.velocityY + 0.09;
+        player.velocityY = player.velocityY + 0.19;
+    }
+
+    if(player.velocityY>=4){
+        player.velocityY = player.velocityY + 0.5;
     }
 
     if(keyDown("LEFT")){
