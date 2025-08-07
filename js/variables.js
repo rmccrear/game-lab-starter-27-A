c56f9bdbdf7dffe93364fcaf657e6efee989915a
Alphonso Gillis <shadowtheguy912@gmail.com>
2025-08-07 08:54:27 -0500

    Resolved Conflicts

--- js/variables.js
+++ js/variables.js
@@ -10,13 +10,9 @@ var platformAnimation3;
 // sprite variables
 var player;
 var star;
-<<<<<<< HEAD
 var platform1;
 var platform2;
 var platform3;
 
 // Boards
 var Score = 0;
-=======
-var score = 0;
->>>>>>> 9bf70b79ebcd041b4fb39eecdd0f7022da08e411
