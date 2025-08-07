ebdc431f31fe9a3f08fa84bdae9d1cd793cb2bd2
Alphonso Gillis <shadowtheguy912@gmail.com>
2025-08-06 20:21:22 -0500

    Added Platforms, without the sprites

--- js/sprite-setup.js
+++ js/sprite-setup.js
@@ -7,6 +7,7 @@ function setupSprites() {
   player.height = 50;
   player.width = 50;
 
+  // Star is to be replaced with food once the sprites come in
   star = createSprite(100, 100, 50, 50);
   star.shapeColor = "aqua";
   // star.addAnimation('twinkle', starAnimation);
@@ -14,4 +15,15 @@ function setupSprites() {
   // star.velocityY = -2;
   // star.height = 30;
   // star.width = 30;
+
+  // Platform Sprites
+  platform1 = createSprite(100, 350, 100, 10);
+  // platform1.addAnimation()
+  platform1.velocityY = -3;
+  platform2 = createSprite(300, 350, 100, 10);
+  // platform2.addAnimation()
+  platform2.velocityY = -3;
+  platform3 = createSprite(200, 250, 100, 10);
+  // platform3.addAnimation()
+  platform3.velocityY = -3;
 }
--- js/variables.js
+++ js/variables.js
@@ -3,6 +3,9 @@
 // animation variables
 var alienAnimation;
 var starAnimation;
+var platformatAnimation1;
+var platformatAnimation2;
+var platformatAnimation3;
 
 // sprite variables
 var player;
