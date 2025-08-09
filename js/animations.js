e6f44ae6540bf0a5d830ece1bd300d7b2aa529d2
Alphonso Gillis <shadowtheguy912@gmail.com>
2025-08-08 21:25:22 -0500

    Formatted for website

--- js/animations.js
+++ js/animations.js
@@ -2,15 +2,15 @@
 function setupAnimations(){
   // Example Code follows.
   // Please delete and replace with your own code.
-  cloudAnimation = loadAnimation("../sprites/cloud.png");
-  friendAnimation = loadAnimation("../sprites/purple.png");
-  fenceAnimation = loadAnimation("../sprites/fence.png");
-  alienAnimation = loadAnimation("../sprites/fly.png");
-  appleAnimation = loadAnimation("../sprites/apple.png");
-  broccoliAnimation = loadAnimation("../sprites/broccoli.png");
-  pizzaAnimation = loadAnimation("../sprites/pizza.png");
-  cakeAnimation = loadAnimation("../sprites/cake.png");
-  platformAnimation1 = loadAnimation("../sprites/plate.png");
-  platformAnimation2 = loadAnimation("../sprites/couch.png");
-  sunAnimation = loadAnimation("../sprites/sun.png");
+  cloudAnimation = loadAnimation("sprites/cloud.png");
+  friendAnimation = loadAnimation("sprites/purple.png");
+  fenceAnimation = loadAnimation("sprites/fence.png");
+  alienAnimation = loadAnimation("sprites/fly.png");
+  appleAnimation = loadAnimation("sprites/apple.png");
+  broccoliAnimation = loadAnimation("sprites/broccoli.png");
+  pizzaAnimation = loadAnimation("sprites/pizza.png");
+  cakeAnimation = loadAnimation("sprites/cake.png");
+  platformAnimation1 = loadAnimation("sprites/plate.png");
+  platformAnimation2 = loadAnimation("sprites/couch.png");
+  sunAnimation = loadAnimation("sprites/sun.png");
 }
